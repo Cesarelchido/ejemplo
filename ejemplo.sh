0c0062ca8bfc59eff12aad3e285f0ab859bcad76
CesarMorales <cesarmoralesvera@gmail.com>
2017-07-25 12:50:24 -0500

    scripts del prope

---
+++ ejemplo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir scripts ;
+touch archivo ;
+mv archivo ./scripts ;
+echo "Hola" > ./scripts/archivo ;
+cat ./scripts/archivo  
---
+++ s1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Inicia el script 1" ;
+ls ;
+mkdir magia ;
+touch alibaba ;
+echo "hocus pocus" ./magia/alibaba ;
+cat ./magia/alibaba ;
+echo "Fin del script1"
